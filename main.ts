e277d860ce479c937610b52ecae94e6f4e3432c4
Robspin <robin@tracefy.com>
2023-07-30 08:43:22 +0200

    added headers

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ import { serve } from "https://deno.land/std@0.155.0/http/server.ts"
 
 const db = await Deno.openKv()
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+  "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+}
+
 serve(async (req: Request) => {
   const pageKey = req.url.split('?key=')[1] ?? ''
   const key = `${pageKey}_views}`
@@ -11,5 +17,5 @@ serve(async (req: Request) => {
   const res = await db.get([key])
   const views = res.value.value
 
-  return new Response(views)
+  return new Response(views, { headers })
 })
