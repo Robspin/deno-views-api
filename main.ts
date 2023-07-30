704c4b93e8427d59516035525860a5b6ea95ea56
Robspin <robin@tracefy.com>
2023-07-30 07:46:44 +0200

    basic views increment functionality

--- main.ts
+++ main.ts
@@ -3,11 +3,13 @@ import { serve } from "https://deno.land/std@0.155.0/http/server.ts"
 const db = await Deno.openKv()
 
 serve(async (req: Request) => {
-  await db.atomic().sum(["views"], 1n).commit()
+  const pageKey = req.url.split('?key=')[1] ?? ''
+  const key = `${pageKey}_views}`
 
-  const res = await db.get(["views"])
-  console.log(res.value.value)
+  await db.atomic().sum([key], 1n).commit()
+
+  const res = await db.get([key])
   const views = res.value.value
 
-  return new Response(`Views: ${views}`)
+  return new Response(views)
 })
