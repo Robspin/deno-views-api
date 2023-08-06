799794259fddc6430721b94327d1fc35fc2a6a65
Robspin <robin@tracefy.com>
2023-08-06 10:40:36 +0200

    Oak implementation + new route

--- main.ts
+++ main.ts
@@ -1,21 +1,48 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts"
+import { Application, Router } from "https://deno.land/x/oak/mod.ts"
 
+const port = 8000
 const db = await Deno.openKv()
+const app = new Application()
 
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-  "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+const setHeaders = (ctx: any) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*")
+  ctx.response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 }
 
-serve(async (req: Request) => {
-  const pageKey = req.url.split('?key=')[1] ?? ''
-  const key = `${pageKey}_views}`
+const router = new Router()
 
-  await db.atomic().sum([key], 1n).commit()
+router.get('/', async (ctx) => {
+  const params = ctx.request.url.search
+  const key = `${params.split('?key=')[1] ?? ''}`
 
-  const res = await db.get([key])
+  setHeaders(ctx)
+
+  await db.atomic().sum(['views', key], 1n).commit()
+
+  const res = await db.get(['views', key])
   const views = res.value.value
 
-  return new Response(views, { headers })
+  console.log(views)
+
+  ctx.response.body = views
 })
+
+router.get('/views', async (ctx) => {
+  setHeaders(ctx)
+
+  const total: { [key: string]: any } = {}
+
+  const entries = await db.list({ prefix: ['views'] })
+  for await (const entry of entries) {
+    const res = await db.get(entry.key)
+    total[String(entry.key[1])] = Number(res.value.value) || 0
+  }
+  ctx.response.body = total
+})
+
+app.use(router.routes())
+
+console.log('Running on port: ', port)
+await app.listen({ port })
+
