5cf4497cecf49859b7af28455c1409f002d088a3
Robspin <robin@tracefy.com>
2023-08-06 10:52:10 +0200

    header test

--- main.ts
+++ main.ts
@@ -4,32 +4,24 @@ const port = 8000
 const db = await Deno.openKv()
 const app = new Application()
 
-const setHeaders = (ctx: any) => {
-  ctx.response.headers.set("Access-Control-Allow-Origin", "*")
-  ctx.response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-  ctx.response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-}
-
 const router = new Router()
 
 router.get('/', async (ctx) => {
   const params = ctx.request.url.search
   const key = `${params.split('?key=')[1] ?? ''}`
 
-  setHeaders(ctx)
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*")
 
   await db.atomic().sum(['views', key], 1n).commit()
 
   const res = await db.get(['views', key])
   const views = res.value.value
 
-  console.log(views)
-
   ctx.response.body = views
 })
 
 router.get('/views', async (ctx) => {
-  setHeaders(ctx)
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*")
 
   const total: { [key: string]: any } = {}
 
